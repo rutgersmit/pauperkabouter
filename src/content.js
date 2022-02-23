0cbd62444e8d29137785a38300c3f0ab44d83ce2
Rutger Smit <rutger.smit@gmail.com>
2022-02-23 22:58:02 +0100

    Observer toegevoegd zodat ook async content wordt gefixt

--- src/content.js
+++ src/content.js
@@ -1,15 +1,32 @@
-var elements = document.getElementsByTagName("*");
-for (var i = 0; i < elements.length; i++) {
-  var element = elements[i];
-  for (var j = 0; j < element.childNodes.length; j++) {
-    var node = element.childNodes[j];
-    if (node.nodeType === 3) {
-      var text = node.nodeValue;
-      var regEx = new RegExp("lil(.*)kleine", "ig");
-      var replacedText = text.replace(regEx, "Pauperkabouter");
-      if (replacedText !== text) {
-        element.replaceChild(document.createTextNode(replacedText), node);
+function wegMetDieNaam() {
+  var elements = document.getElementsByTagName("*");
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+      if (node.nodeType === 3) {
+        var text = node.nodeValue;
+        var regEx = new RegExp("lil(.*)kleine", "igm");
+        var replacedText = text.replace(regEx, "Pauperkabouter");
+        if (replacedText !== text) {
+          element.replaceChild(document.createTextNode(replacedText), node);
+        }
       }
     }
   }
 }
+
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
+
+const callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    console.log(mutation.type);
+    if (mutation.type === "childList") {
+      wegMetDieNaam();
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
